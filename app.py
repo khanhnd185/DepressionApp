ffa1eaf74a16412a8469cc174b67d6f6786e70f4
Khanh Nguyen <khanhnd185@gmail.com>
2023-05-16 00:56:45 +0700

    Update camera feature

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import cv2
 import threading
 import webbrowser
 
 from pytube import YouTube
 from tkinter import *
 from voicebot import voicebot
+from PIL import ImageTk, Image
 
 BG_TEXT = "#2C3E50"
 BG_GRAY = "#ABB2B9"
@@ -11,6 +13,7 @@ BG_COLOR = "#17202A"
 TEXT_COLOR = "#EAECEE"
 FONT = "Helvetica 14"
 FONT_BOLD = "Helvetica 13 bold"
+FONT_TITLE = "Helvetica 14 bold"
 
 # GUI class for the chat
 class GUI:
@@ -20,101 +23,135 @@ class GUI:
         # chat window which is currently hidden
         self.Window = Tk()
         self.Window.withdraw()
- 
-        # login window
-        self.login = Toplevel()
-        self.login.title("Depression Recognition Application")
-        self.login.resizable(width=False, height=False)
-        self.login.configure(width=400, height=300)
-
-        self.title = Label(self.login,
-                         text="Depression Recognition Application",
-                         justify=CENTER,
-                         font=FONT)
- 
-        self.title.place(relheight=0.15, relx=0.2, rely=0.07)
-
-        # create buttons
-        self.but1 = Button(self.login,
-                         text="YOUTUBE",
-                         font=FONT_BOLD,
-                         command=lambda: self.youtube())
-        self.but1.place(relx=0.3, rely=0.55)
-
-        self.but2 = Button(self.login,
-                         text="INTERVIEW",
-                         font=FONT_BOLD,
-                         command=lambda: self.interview())
-        self.but2.place(relx=0.6, rely=0.55)
+        self.init_homepage()
 
         #Start main loop
         self.Window.mainloop()
 
-    # Send function
+    def init_homepage(self):
+        # login window
+        self.login = Toplevel()
+        self.login.title("Depression Recognition Application")
+        self.login.resizable(width=False, height=False)
+        self.login.configure(width=450, height=450, bg=BG_COLOR)
+        self.title = Label(self.login
+                         , bg=BG_COLOR
+                         , fg=TEXT_COLOR
+                         , font=FONT_TITLE 
+                         , text="Depression Recognition Application"
+                         , justify=CENTER)
+        self.title.place(anchor='center', relx=0.5, rely=0.1)
+
+        self.frame = Frame(self.login, width=300, height=300)
+        self.frame.pack()
+        self.frame.place(anchor='center', relx=0.5, rely=0.5)
+        self.img = ImageTk.PhotoImage(Image.open("logo.png"))
+        self.logo = Label(self.frame, image = self.img)
+        #self.logo.grid(row=1, column=0, columnspan=2)
+        self.logo.pack()
+
+        # create buttons
+        self.but1 = Button(self.login
+                        ,text="YOUTUBE"
+                        ,font=FONT_BOLD
+                        ,command=lambda: self.from_home_to_youtube())
+        self.but1.place(anchor='center', relx=0.25, rely=0.93)
+
+        self.but2 = Button(self.login
+                        , text="INTERVIEW"
+                        , font=FONT_BOLD
+                        , command=lambda: self.from_home_to_interview())
+        self.but2.place(anchor='center', relx=0.72, rely=0.93)
+
+        self.but2 = Button(self.login
+                        , text="CAMERA"
+                        , font=FONT_BOLD
+                        , command=lambda: self.from_home_to_camera())
+        self.but2.place(anchor='center', relx=0.48, rely=0.93)
+
+    def deinit_homepage(self):
+        self.but2.destroy()
+        self.but1.destroy()
+        self.frame.destroy()
+        self.logo.destroy()
+        self.title.destroy()
+        self.login.destroy()
+
+    def from_home_to_interview(self):
+        self.deinit_homepage()
+        self.init_interview_page()
+
     def start_interview(self):
         # Start the handling thread
         thread = threading.Thread(target=lambda: voicebot(self.chatscreen))
         thread.start()
 
-    def interview(self):
-        self.login.destroy()
-        self.layout_interview()
-
-    def layout_interview(self):
+    def init_interview_page(self):
         # to show chat window
-        self.Window.deiconify()
-        self.Window.title("Depression Interview")
-        self.Window.resizable(width=False,
-                              height=False)
-        self.Window.configure(width=470,
-                              height=550,
-                              bg=BG_COLOR)
+        self.interview = Toplevel()
+        self.interview.title("Depression Interview")
+        self.interview.resizable(width=False, height=False)
+        self.interview.configure(width=470, height=550, bg=BG_COLOR)
         
         
-        self.title_interview = Label(self.Window
+        self.title_interview = Label(self.interview
                                 , bg=BG_COLOR
                                 , fg=TEXT_COLOR
                                 , text="Depression Interview"
                                 , font=FONT_BOLD
                                 , pady=10, width=20
-                                , height=1).grid(row=0)
+                                , height=1)
+        self.title_interview.grid(row=0, column=0, columnspan=2)
  
-        self.chatscreen = Text(self.Window, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=60)
+        self.chatscreen = Text(self.interview, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=60)
         self.chatscreen.grid(row=1, column=0, columnspan=2)
         
         self.scrollbar = Scrollbar(self.chatscreen)
         self.scrollbar.place(relheight=1, relx=0.974)
-        
-        self.start_interview_but = Button(self.Window, text="Start", font=FONT_BOLD, bg=BG_GRAY,
-                    command=self.start_interview).grid(row=2, column=1)
+    
+        self.start_interview_but = Button(self.interview, text="Start", font=FONT_BOLD, bg=BG_GRAY,
+                    command=self.start_interview)
+        self.start_interview_but.grid(row=2, column=0)
+
+        self.from_interview_to_home_but = Button(self.interview, text="Home", font=FONT_BOLD, bg=BG_GRAY,
+                    command=self.from_interview_to_home)
+        self.from_interview_to_home_but.grid(row=2, column=1)
+
+    def deinit_interview_page(self):
+        self.from_interview_to_home_but.destroy()
+        self.start_interview_but.destroy()
+        self.scrollbar.destroy()
+        self.title_interview.destroy()
+        self.interview.destroy()
+
+    def from_interview_to_home(self):
+        self.deinit_interview_page()
+        self.init_homepage()
 
 
-    def youtube(self):
-        self.login.destroy()
-        self.layout_youtube()
+    def from_home_to_youtube(self):
+        self.deinit_homepage()
+        self.init_youtube_page()
 
-    def layout_youtube(self):
+    def init_youtube_page(self):
         # to show chat window
         self.link = None
-        self.Window.deiconify()
-        self.Window.title("Youtube Depression Recognition")
-        self.Window.resizable(width=False,
-                              height=False)
-        self.Window.configure(width=470,
-                              height=550,
-                              bg=BG_COLOR)
+        self.youtube = Toplevel()
+        self.youtube.title("Youtube Depression Recognition")
+        self.youtube.resizable(width=False, height=False)
+        self.youtube.configure(width=470, height=550, bg=BG_COLOR)
         
-        
-        self.title_youtube = Label(self.Window
+        self.title_youtube = Label(self.youtube
                                 , bg=BG_COLOR
                                 , fg=TEXT_COLOR
                                 , text="Youtube Depression Recognition"
                                 , font=FONT_BOLD
-                                , pady=10, width=30
-                                , height=1).grid(row=0)
+                                , width=30
+                                , height=1)
+        self.title_youtube.grid(row=0, column=0, columnspan=2)
  
         self.string_var = StringVar()
-        self.video_title = Label(self.Window
+        self.video_title = Label(self.youtube
                                 , bg=BG_COLOR
                                 , fg=TEXT_COLOR
                                 , text="None"
@@ -122,15 +159,31 @@ class GUI:
                                 , pady=10
                                 , height=1
                                 , textvariable=self.string_var)
-        self.video_title.grid(row=1)
+        self.video_title.grid(row=1, column=0, columnspan=2)
         self.video_title.bind("<Button-1>", lambda e: self.callback(self.link))
 
-        self.e = Entry(self.Window, bg=BG_TEXT, fg=TEXT_COLOR, font=FONT, width=55)
-        self.e.grid(row=3, column=0)
-        
-        self.check_video_but = Button(self.Window, text="Check", font=FONT_BOLD, bg=BG_GRAY,
-                    command=lambda: self.read_link(self.e.get())).grid(row=3, column=1)
+        self.e = Entry(self.youtube, bg=BG_TEXT, fg=TEXT_COLOR, font=FONT, width=55)
+        self.e.grid(row=2, column=0, columnspan=2)
+
+        self.check_video_but = Button(self.youtube, text="Check", font=FONT_BOLD, bg=BG_GRAY,
+                    command=lambda: self.read_link(self.e.get()))
+        self.check_video_but.grid(row=3, column=0)
         self.string_var.set("Input your YouTube URL")
+
+        self.from_youtube_to_home_but = Button(self.youtube, text="Home", font=FONT_BOLD, bg=BG_GRAY,
+                    command=self.from_youtube_to_home)
+        self.from_youtube_to_home_but.grid(row=3, column=1)
+
+    def deinit_youtube_page(self):
+        self.from_youtube_to_home_but.destroy()
+        self.check_video_but.destroy()
+        self.e.destroy()
+        self.title_youtube.destroy()
+        self.youtube.destroy()
+
+    def from_youtube_to_home(self):
+        self.deinit_youtube_page()
+        self.init_homepage()
 
     def callback(self, url):
         webbrowser.open_new(url)
@@ -144,7 +197,55 @@ class GUI:
         self.string_var.set(yt.title)
         self.e.delete(0, END)
 
+    ## Test feature camera
+    def from_home_to_camera(self):
+        self.deinit_homepage()
+        self.init_camera_page()
 
- 
+    def open_camera(self):
+        _, frame = self.vid.read()
+        opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        captured_image = Image.fromarray(opencv_image)
+        photo_image = ImageTk.PhotoImage(image=captured_image)
+        self.label_widget.photo_image = photo_image
+        self.label_widget.configure(image=photo_image)
+        self.label_widget.after(10, self.open_camera)
+
+    def init_camera_page(self):
+        
+        self.vid = cv2.VideoCapture(0)
+        # Declare the width and height in variables
+        width, height = 800, 600
+        
+        # Set the width and height
+        self.vid.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.vid.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+        # to show chat window
+        self.camera = Toplevel()
+        self.camera.title("Depression Camera")
+
+        self.label_widget = Label(self.camera)
+        self.label_widget.pack()
+    
+        self.start_camera_but = Button(self.camera, text="Start", font=FONT_BOLD, bg=BG_GRAY,
+                    command=self.open_camera)
+        self.start_camera_but.pack()
+
+        self.from_camera_to_home_but = Button(self.camera, text="Home", font=FONT_BOLD, bg=BG_GRAY,
+                    command=self.from_camera_to_home)
+        self.from_camera_to_home_but.pack()
+
+    def deinit_camera_page(self):
+        self.from_camera_to_home_but.destroy()
+        self.start_camera_but.destroy()
+        self.label_widget.destroy()
+        self.camera.destroy()
+
+    def from_camera_to_home(self):
+        self.deinit_camera_page()
+        self.init_homepage()
+
+
 # create a GUI class object
 g = GUI()
